fdbe31a744e2d97046258125a4c2348a7cb2a9b3
Oscar Zamudio <zamudio_1526@hotmail.com>
2020-06-11 11:02:38 -0700

    Cleaned up formats

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-'use strict';
+"use strict";
 
 function showProjects() {
-    $('#projects-button').on('click', function (event) {
-        $('#bio-screen').hide();
-        $('#projects-screen').fadeIn();
-    });
-};
-
+  $("#projects-button").on("click", function (event) {
+    $("#bio-screen").hide();
+    $("#projects-screen").fadeIn();
+  });
+}
 
 function showContacts() {
-   $('#contact-button').on('click', function (event) {
-        $('#contact-screen').fadeIn();
-        $('#bio-screen').hide();
-    });
-};
-
+  $("#contact-button").on("click", function (event) {
+    $("#contact-screen").fadeIn();
+    $("#bio-screen").hide();
+  });
+}
 
 function handlePortfolioApp() {
-    showProjects();
-    showContacts();
-};
+  showProjects();
+  showContacts();
+}
 
 $(handlePortfolioApp);
